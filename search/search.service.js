4f30faa66f96d698757254def66ff2af801ffa60
Giovanni A. Camacho <gcamacho79@gmail.com>
2018-03-02 17:02:27 -0500

    Cleaned up the SearchService

--- search/search.service.js
+++ search/search.service.js
@@ -5,44 +5,40 @@
     .service('SearchService', ['$http', function($http) {
 
       // Local variables
-      const searchUrl = "https://api.github.com/search/users?q=";
-      const userUrl = 'https://api.github.com/users';
+      const url = 'https://api.github.com';
+      const searchUrl = `${url}/search/users?q=`;
+      const userUrl = `${url}/users`;
 
       /**
        * Perform a ajax call to search github users
        * @param {string} searchTerm
        * @returns Promise
        */
-      function search(searchTerm = '') {
+      const search = (searchTerm = '') => {
         if (searchTerm) {
-          return $http.get(searchUrl + searchTerm);
+          return $http.get(`${searchUrl}${searchTerm}`);
         }
-      }
+      };
 
       /**
        * Peform a ajax call to fetch the user's Repositories
        * @param {string} repoUrl
        * @returns Promise
        */
-      function fetchRepos(repoUrl) {
-        return $http.get(repoUrl);
-      }
+      const fetchRepos = (repoUrl) => $http.get(repoUrl);
 
       /**
        * Peform a ajax call to fetch the user's gists
        * @param {string} login
        * @returns Promise
        */
-      function fetchGists(login) {
-        return $http.get(`${userUrl}/${login}/gists`);
-      }
-
+      const fetchGists = (login) => $http.get(`${userUrl}/${login}/gists`);
 
       // Public API
       return {
-        search: search,
-        fetchRepos: fetchRepos,
-        fetchGists: fetchGists
+        search,
+        fetchRepos,
+        fetchGists
       }
 
     }
