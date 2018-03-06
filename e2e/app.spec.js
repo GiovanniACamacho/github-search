2bf94e27531d2be2912fe8cca97c21e7e9f55d40
Giovanni A. Camacho <gcamacho79@gmail.com>
2018-03-06 07:20:41 -0500

    More protractor tests

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -10,4 +10,47 @@ describe('Github Search - E2E', function() {
     var user = element(by.css('.github-user'));
     expect(user.isPresent()).toBe(true); 
   });
+
+  describe('user repos', function() {
+    var repos;
+    beforeEach(function() {
+      var input = element(by.model('searchTerm'));
+      input.sendKeys('google', protractor.Key.ENTER);
+      repos = element(by.css('.repos-list'));
+    });
+    it('should be visible', function() {
+      expect(repos.isPresent()).toBe(true); 
+    });
+    it('should be populated with data', function() {
+      var items = repos.$$('div');
+      expect(items.count()).toBeGreaterThan(0);
+    });
+    it('should linkable', function() {
+      var item = repos.$$('div').get(0);
+      var a = item.$$('a');
+      expect(a.getAttribute('href')).toMatch(/https:\/\/github\.com/);
+    });
+  });
+
+  describe('user gists', function() {
+    var gists;
+    beforeEach(function() {
+      var input = element(by.model('searchTerm'));
+      input.sendKeys('xtea', protractor.Key.ENTER);
+      gists = element(by.css('.gists-list'));
+    });
+    it('should be visible', function() {
+      expect(gists.isPresent()).toBe(true); 
+    });
+    it('should be populated with data', function() {
+      var items = gists.$$('div');
+      expect(items.count()).toBeGreaterThan(0);
+    });
+    it('should linkable', function() {
+      var item = gists.$$('div').get(0);
+      var a = item.$$('a');
+      expect(a.getAttribute('href')).toMatch(/https:\/\/gist.github\.com/);
+    });
+  });
+
 });
